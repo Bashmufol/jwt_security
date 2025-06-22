30bbdb21e1b0bfff347a660e9f599947065804bb
Bashir Muhammed <bashmufol@gmail.com>
2025-06-22 18:23:25 +0100

    my first commit

---
+++ src/main/java/com/bash/springsecuritydemo/model/User.java
@@ -0,0 +1,22 @@
+package com.bash.springsecuritydemo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
