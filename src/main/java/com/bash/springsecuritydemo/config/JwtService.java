3ad286dcb5559d3c3d6f59f6fd96712b4fb30fc0
Bashir Muhammed <bashmufol@gmail.com>
2025-06-23 22:36:22 +0100

    claims extracted

--- src/main/java/com/bash/springsecuritydemo/config/JwtService.java
+++ src/main/java/com/bash/springsecuritydemo/config/JwtService.java
@@ -2,17 +2,38 @@ package com.bash.springsecuritydemo.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String secretKey = "FlBeNjqCjBgz1itEGaNjQFfQ44pm0A3zoOxcviqWoSrMZBAYsisvLOyjz6vL8BXG\n";
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+    public String generateToken(Map<String, Object> claims,
+                                UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token) {
@@ -27,5 +48,6 @@ public class JwtService {
 
     private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
