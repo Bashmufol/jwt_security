660cbf1fd8ee2a2c1d35913f99be24ff34acff7a
Bashir Muhammed <bashmufol@gmail.com>
2025-06-23 23:56:23 +0100

    UserDetailsService added

--- src/main/java/com/bash/springsecuritydemo/model/UserRepository.java
+++ src/main/java/com/bash/springsecuritydemo/model/UserRepository.java
@@ -1,9 +1,11 @@
 package com.bash.springsecuritydemo.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> fingByEmail(String email);
+    Optional<User> findByEmail(String email);
+
 }
