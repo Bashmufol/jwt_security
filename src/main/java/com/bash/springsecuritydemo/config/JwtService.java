c05dbb3888f2af914d944b5672a09c6bc44019c1
Bashir Muhammed <bashmufol@gmail.com>
2025-06-23 23:25:29 +0100

    Token Validation mehtod added

--- src/main/java/com/bash/springsecuritydemo/config/JwtService.java
+++ src/main/java/com/bash/springsecuritydemo/config/JwtService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -24,6 +25,14 @@ public class JwtService {
         final Claims claims = extractAllClaims(token);
         return claimResolver.apply(claims);
     }
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()));
+    }
+
     public String generateToken(Map<String, Object> claims,
                                 UserDetails userDetails) {
         return Jwts
