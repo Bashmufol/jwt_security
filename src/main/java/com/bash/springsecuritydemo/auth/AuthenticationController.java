5cf1ab08aa08cf2001858ac7d9e3290387ba7813
Bashir Muhammed <bashmufol@gmail.com>
2025-06-24 15:31:21 +0100

    Controller created

---
+++ src/main/java/com/bash/springsecuritydemo/auth/AuthenticationController.java
@@ -0,0 +1,24 @@
+package com.bash.springsecuritydemo.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthenticationController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
+
+    }
+}
