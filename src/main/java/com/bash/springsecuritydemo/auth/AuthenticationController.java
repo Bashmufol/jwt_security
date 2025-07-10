da36fc72cb6575b77fd4f0f45145ba2bb15d4b76
Bashir Muhammed <bashmufol@gmail.com>
2025-07-10 20:41:42 +0100

    updated

--- src/main/java/com/bash/springsecuritydemo/auth/AuthenticationController.java
+++ src/main/java/com/bash/springsecuritydemo/auth/AuthenticationController.java
@@ -21,10 +21,9 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
-//this is 3 and 4
+//this is 3
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
     }
-
 }
