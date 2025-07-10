f67e49f3bb53b94705f433224730726cd42e45f8
Bashir Muhammed <bashmufol@gmail.com>
2025-07-10 20:42:45 +0100

    updated

--- src/main/java/com/bash/springsecuritydemo/auth/AuthenticationController.java
+++ src/main/java/com/bash/springsecuritydemo/auth/AuthenticationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
-//
+
     private final AuthenticationService authService;
 
     @PostMapping("/register")
